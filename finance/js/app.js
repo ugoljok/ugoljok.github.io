1e189cfb83e6256ca543fa020ad86f332c5cd7b5
ugoljok <ugoljok91@gmail.com>
2017-04-02 23:05:07 +0300

    тест чтение запись

--- finance/js/app.js
+++ finance/js/app.js
@@ -2,6 +2,7 @@ var app={};
 app.uid= null //идентификатор пользователя
 //тщчка инициализации приложения
 app.init = function (user){
+	log(user)
 	app.uid = user.uid; //пишем идентификатор в глобальный объект
 	//получаем данные пользователя и выводим на экран
 	document.getElementById('user').innerHTML = '<img src="' +user.photo+ '" width="50"><br>' +user.name;
