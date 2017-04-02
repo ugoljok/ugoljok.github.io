6ae0a71ceef4ab8f20737517c35de0d9d86e64cb
ugoljok <ugoljok91@gmail.com>
2017-04-02 20:49:41 +0300

    тест чтение запись

--- finance/js/auth.js
+++ finance/js/auth.js
@@ -1,37 +1,37 @@
 // Initialize Firebase
-var config = {
-	apiKey: "AIzaSyBSUnWiQcL7R12d_IdGi0_0z3rAIuzKyvw",
-	authDomain: "finance-ed0a6.firebaseapp.com",
-	databaseURL: "https://finance-ed0a6.firebaseio.com",
-	storageBucket: "finance-ed0a6.appspot.com",
-	messagingSenderId: "387748315109"
-};
-firebase.initializeApp(config);
+	var config = {
+		apiKey: "AIzaSyBSUnWiQcL7R12d_IdGi0_0z3rAIuzKyvw",
+		authDomain: "finance-ed0a6.firebaseapp.com",
+		databaseURL: "https://finance-ed0a6.firebaseio.com",
+		storageBucket: "finance-ed0a6.appspot.com",
+		messagingSenderId: "387748315109"
+	};
+	firebase.initializeApp(config); //задаем параметры firebase
+	// событие изменения статуса авторизации
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+		  // User is signed in.
+			app.init(user);
+			console.log(user)
+		} else {
+		  // No user is signed in.
+			var provider = new firebase.auth.GoogleAuthProvider();
+				//provider.addScope('https://www.googleapis.com/auth/plus.login');
+				firebase.auth().signInWithPopup(provider).then(function(result) {
+				  // This gives you a Google Access Token. You can use it to access the Google API.
+				  var token = result.credential.accessToken;
+				  // The signed-in user info.
+				  var user = result.user;
 
-firebase.auth().onAuthStateChanged(function(user) {
-	if (user) {
-	  // User is signed in.
-		document.getElementById('user').innerHTML = '<img src="' +user.photoURL+ '"> '+user.displayName;
-		console.log(user)
-	} else {
-	  // No user is signed in.
-		var provider = new firebase.auth.GoogleAuthProvider();
-			//provider.addScope('https://www.googleapis.com/auth/plus.login');
-			firebase.auth().signInWithPopup(provider).then(function(result) {
-			  // This gives you a Google Access Token. You can use it to access the Google API.
-			  var token = result.credential.accessToken;
-			  // The signed-in user info.
-			  var user = result.user;
-
-			}).catch(function(error) {
-			  // Handle Errors here.
-			  var errorCode = error.code;
-			  var errorMessage = error.message;
-			  // The email of the user's account used.
-			  var email = error.email;
-			  // The firebase.auth.AuthCredential type that was used.
-			  var credential = error.credential;
-			  // ...
-			});
-	}
-});
+				}).catch(function(error) {
+				  // Handle Errors here.
+				  var errorCode = error.code;
+				  var errorMessage = error.message;
+				  // The email of the user's account used.
+				  var email = error.email;
+				  // The firebase.auth.AuthCredential type that was used.
+				  var credential = error.credential;
+				  // ...
+				});
+		}
+	});
